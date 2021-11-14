a0adcdce3875095e5a37647734a02ef162c672b4
madlur <2185430@gmail.com>
2021-11-14 22:01:25 +0300

    Задание +++1. Разобраться с примером проекта на Spring MVC; +++(был сделан на прошлом уроке, не попал в пул риквест) 2. Создать класс Товар (Product), с полями id, title, cost; +++(был сделан на прошлом уроке, не попал в пул риквест) 3. Товары необходимо хранить в репозитории (класс, в котором в виде List<Product> хранятся товары) +++4. Сделать страницу, на которой отображаются все товары из репозитория.

--- src/main/java/ru/geekbrains/context/MainApp.java
+++ src/main/java/ru/geekbrains/context/MainApp.java
@@ -1,61 +1,12 @@
 package ru.geekbrains.context;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
 
+@SpringBootApplication
 public class MainApp {
-
-    private static Logger logger = LoggerFactory.getLogger(MainApp.class);
-
     public static void main(String[] args) {
-
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("ru.geekbrains.context");
-        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
-        String command;
-        ProductRepository productRepository = context.getBean("produktik", ProductRepository.class);
-        try {
-            while (!(command = buff.readLine()).equals("exit")) {
-                if (command.equals("show")) {
-                    System.out.println("Введите ID продукта для вывода его на экран ");
-                    long id = Long.parseLong(buff.readLine());
-                    System.out.println(productRepository.findById(id));
-                }
-                if (command.equals("get all")) {
-                    List<Product> products = productRepository.findAllProducts();
-                    for (Product p : products) {
-                        System.out.println(p);
-                    }
-
-                }
-                if (command.equals("add cart")) {
-                    System.out.println("Введите ID продукта для добавления в корзину");
-                    long id = Long.parseLong(buff.readLine());
-                    Cart cart = context.getBean(Cart.class);
-                    cart.add(id);
-                    cart.showcart();
-
-                }
-
-                if (command.equals("delete cart")) {
-                    System.out.println("Введите ID продукта для удаления");
-                    long id = Long.parseLong(buff.readLine());
-                    Cart cart = context.getBean(Cart.class);
-                    cart.delete(id);
-                    cart.showcart();
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        context.close();
+        SpringApplication.run(MainApp.class, args);
     }
-
 }
